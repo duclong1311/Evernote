05c9d86a221990486703830043b88fc1d96a7ad6
duclong1311 <duclong1311.work@gmail.com>
2024-03-07 15:39:25 +0700

    Model
    creating model

---
+++ Evernote Application/Model/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evernote_Application.Model
+{
+    internal class User
+    {
+    }
+}
